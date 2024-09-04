40db249635c4cd66fe96df54d98f560a4a124818
athithyaramaa1 <athithya.ramma@kalvium.community>
2024-09-04 10:01:39 +0530

    chore: Implement Static Variables Concept in JAVA OOP

--- Tools.java
+++ Tools.java
@@ -2,11 +2,17 @@ public class Tools {
     private String toolName;
     private boolean isElectric;
     private String purpose;
+    private static int toolCount = 0;
 
     public Tools(String toolName, boolean isElectric, String purpose){
         this.toolName = toolName;
         this.isElectric = isElectric;
         this.purpose = purpose;
+        toolCount++;
+    }
+
+    static int getToolCount() {
+        return toolCount;
     }
     
     void helloTool(){
